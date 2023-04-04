91514a86570488e1746829e592e3a159fe143158
Silas Vasconcelos <75582929+s-v7@users.noreply.github.com>
2023-04-04 17:01:49 -0300

    Update methods
    Add methods gets, sets

--- Models/MachineIot.cs
+++ Models/MachineIot.cs
@@ -3,12 +3,25 @@ namespace MachineWebApi.Models;
 public class MachineIot
 {
     public string Id { get; set; }
-
-    public string Name { get; set; }
-
-    public bool IsComplete { get; set; }
-
-    public string Secret { get; set; }
+    
+    private string _name;
+    public string Name 
+    { 
+        get => _name; 
+        set => _name =  value; 
+    }
+    private bool _isComlete;
+    public bool IsComplete
+    { 
+        get => _isComlete; 
+        set => _isComlete =  value; 
+    }
+    private string _secret;
+    public string Secret
+    { 
+        get => _secret; 
+        set => _secret = value; 
+    }
 
     public MachineIot(string id, string name, bool isComplete, string secret)
     {
